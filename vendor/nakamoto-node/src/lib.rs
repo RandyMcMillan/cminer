9dc23b95e68e5aa18d01c7684b415adcb43406f2
randymcmillan <randymcmillan@protonmail.com>
2026-07-23 16:27:49 -0400

    vendor/nakamoto-node

---
+++ vendor/nakamoto-node/src/lib.rs
@@ -0,0 +1,43 @@
+//! Stand-alone light-client daemon. Runs the light-client as a background process.
+#![deny(missing_docs, unsafe_code)]
+
+use std::net;
+use std::path::PathBuf;
+
+pub use nakamoto_client::{Client, Config, Error, Network};
+pub use nakamoto_client::{Domain, LoadingHandler};
+
+pub mod logger;
+
+/// The network reactor we're going to use.
+type Reactor = nakamoto_net_poll::Reactor<net::TcpStream>;
+
+/// Run the light-client. Takes an initial list of peers to connect to, a list of listen addresses,
+/// the client root and the Bitcoin network to connect to.
+pub fn run(
+    connect: &[net::SocketAddr],
+    listen: &[net::SocketAddr],
+    root: Option<PathBuf>,
+    domains: &[Domain],
+    network: Network,
+) -> Result<(), Error> {
+    let mut cfg = Config {
+        network,
+        connect: connect.to_vec(),
+        domains: domains.to_vec(),
+        listen: if listen.is_empty() {
+            vec![([0, 0, 0, 0], 0).into()]
+        } else {
+            listen.to_vec()
+        },
+        ..Config::default()
+    };
+    if let Some(path) = root {
+        cfg.root = path;
+    }
+    if !connect.is_empty() {
+        cfg.limits.max_outbound_peers = connect.len();
+    }
+
+    Client::<Reactor>::new()?.run(cfg)
+}
