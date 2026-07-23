42c05a9adeab80d8cebd4bd1cc8822dbd55468b9
randymcmillan <randymcmillan@protonmail.com>
2026-07-23 17:26:10 -0400

    src/nakamoto.rs:remove

--- src/nakamoto.rs
+++
@@ -1,9 +0,0 @@
-use nakamoto_client::handle::{Error, Handle};
-use nakamoto_common::block::Transaction;
-
-pub fn mempool_snapshot<H>(handle: &H) -> Result<Vec<Transaction>, Error>
-where
-    H: Handle,
-{
-    handle.mempool()
-}
